ce508c950d2f4f7b81336b62cc5c8ed4e263bb0c
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-05-31 17:47:04 +0700

    membuat fungsi mengambil nilai dari inputan

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.getElementById('surveyForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
+    var name = document.getElementById('name').value;
+    var dob = document.getElementById('dob').value;
+    var favoriteAlbum = document.getElementById('favoriteAlbum').value;
+    var concert = document.querySelector('input[name="concert"]:checked').value;
+    var favoriteSongsArray = Array.from(document.querySelectorAll('input[name="favoriteSongs"]:checked'))
+                                  .map(checkbox => checkbox.value);
+    var favoriteSongs = favoriteSongsArray.join(', ');
+    var otherSong = document.getElementById('otherSong').value;
+    var comments = document.getElementById('comments').value;
     });
